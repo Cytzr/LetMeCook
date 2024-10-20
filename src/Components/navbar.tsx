fb2b49d361d1ea421c26788eaeb86fe98d0bc5bc
Cytzr <kelvinethanaelyahja@gmail.com>
2024-10-20 11:40:05 +0700

    feat: navbar layouting

--- src/Components/navbar.tsx
+++ src/Components/navbar.tsx
@@ -1,7 +1,14 @@
+import { useLocation } from 'react-router-dom';
+import '../Styles/navbar-style.css'
+
 function CustomNavbar() {
+    const location = useLocation();
+
+    console.log(location);
+
     return (
-        <div className="container-fluid">
-            <div className="row d-flex justify-content-between align-items-center text-center">
+        <div className="container-fluid main-container">
+            <div className="row d-flex justify-content-between align-items-center text-center p-10">
                 <div className="col-8">
                     <div className="row dp-flex align-items-center fw-bold">
                         <div className="col">
@@ -9,17 +16,44 @@ function CustomNavbar() {
                                 <img src="..\src\Images\AppIconHalf.png" alt="" height={40} />
                             </a>
                         </div>
-                        <div className="col">Recipes</div>
-                        <div className="col">Ingredients</div>
-                        <div className="col">Our Experts</div>
-                        <div className="col">About Us</div>
+                        <div className="col">
+                            <button className='navbar-button'>Recipes</button>
+                        </div>
+                        <div className="col">
+                            <button className='navbar-button'>Ingredients</button>
+                        </div>
+                        <div className="col">
+                            <button className='navbar-button'>Our Experts</button>
+                        </div>
+                        <div className="col">
+                            <button className='navbar-button'>About Us</button>
+                        </div>
                     </div>
                 </div>
                 <div className="col-4">
                     <div className="row dp-flex align-items-center">
-                        <div className="col"><img src="..\src\Images\SaveIcon.png" alt="" /></div>
-                        <div className="col">Sign in</div>
-                        <div className="col">Premium</div>
+                        <div className="col"></div>
+
+                        {/* conditional rendering based on local storage not available */}
+                        {/* <div className="col-10 d-flex align-items-center justify-content-end mr-1">Sign in</div>
+                        <div className="col-1"></div> */}
+
+                        {/* conditional rendering based on local storage available */}
+                        <div className="col">
+                            <button className='premium-button'>
+                                Premium
+                            </button>
+                        </div>
+                        <div className="col d-flex flex-row align-items-center justify-content-end">
+                            <div className="col-2">
+                                <button className='save-button'>
+                                    <img src="..\src\Images\SaveIcon.png" alt="" className="img-fluid save-icon" />
+                                </button>
+                            </div>
+                            <div className="col-10">
+                                <button className='username-button'>Username</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
