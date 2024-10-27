6e9e4de014a3e5108d555ca90f235b14bc39184a
Cytzr <kelvinethanaelyahja@gmail.com>
2024-10-27 10:38:58 +0700

    feat: recipes page finishing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Footer from "./Components/footer"
 import CustomNavbar from "./Components/navbar"
-import YouMightLikeCard from "./Components/you-might-like-card"
 
 function App() {
   return (
     <>
       <CustomNavbar />
-      <YouMightLikeCard FoodID={"1"} FoodName={"Honey Chicken"} FoodDescription={"Start your day with a bowl of wholesome, creamy porridge made from premium grains and cooked to perfection."} FoodCookTime={20} ImageLink={"../src/Images/HoneyChicken.png"} />
       <Footer />
     </>
   )
