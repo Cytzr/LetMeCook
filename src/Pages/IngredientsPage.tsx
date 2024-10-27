caaf65197806fcef97031d0b12bed3254e014ee3
Cytzr <kelvinethanaelyahja@gmail.com>
2024-10-27 19:44:50 +0700

    feat: Ingredients Page WIP

--- src/Pages/IngredientsPage.tsx
+++ src/Pages/IngredientsPage.tsx
@@ -3,8 +3,8 @@ import { Col, Container, Row } from "react-bootstrap";
 import CustomNavbar from "../Components/navbar";
 import { useState } from "react";
 import IngredientButtonProps from "../Interfaces/ingredients-button-props";
-import IngredientsCardProps from "../Interfaces/ingredients-card-props";
 import IngredientsCard from "../Components/ingredients-card";
+import { IngredientProps } from "../Interfaces/ingredients-card-props";
 
 const IngredientItems: IngredientButtonProps[] = [
     { Label: "Protein", LinkIcon: "../src/Images/protein-icon.png", ItemNumber: 1 },
@@ -16,21 +16,80 @@ const IngredientItems: IngredientButtonProps[] = [
     { Label: "Water", LinkIcon: "../src/Images/water-icon.png", ItemNumber: 7 },
 ];
 
-const TempData: IngredientsCardProps[] = [
-    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"] },
-    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"] },
-    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"] },
-    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"] },
-    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"] },
-    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"] },
-    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"] },
-    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"] },
-    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"] },
-    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"] }
+const TempData = [
+    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"], IngredientID: "1" },
+    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"], IngredientID: "2" },
+    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"], IngredientID: "3" },
+    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"], IngredientID: "4" },
+    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"], IngredientID: "5" },
+    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"], IngredientID: "6" },
+    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"], IngredientID: "7" },
+    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"], IngredientID: "8" },
+    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"], IngredientID: "9" },
+    { IngredientWeightPerPorsion: 33, TotalCalorie: 13, ImageLink: "../src/Images/HoneyChicken.png", IngredientName: "Beef", IngredientDescription: "Beef is the meat derived from cattle and is one of the most widely consumed proteins worldwide.", NutrientsContained: ["Proteins", "Vitamins", "Fats"], IngredientID: "10" }
 ];
 
 function IngredientsPage() {
     const [Selected, setSelected] = useState(0);
+
+    const [Cart, setCart] = useState<IngredientProps[]>([]);
+
+    // const cartChange = (Ingredient: IngredientProps, Action: string) => {
+    //     setCart((prevCart) => {
+    //         const existingItemIndex = prevCart.findIndex((item) => item.IngredientID === Ingredient.IngredientID);
+
+    //         if (existingItemIndex >= 0) {
+    //             const updatedCart = [...prevCart];
+    //             const existingItem = updatedCart[existingItemIndex];
+    //             const newAmount = Action === "Add" ? existingItem.IngredientAmount + 1 : Math.max(existingItem.IngredientAmount - 1, 0);
+
+    //             updatedCart[existingItemIndex] = {
+    //                 ...existingItem,
+    //                 IngredientAmount: newAmount
+    //             };
+    //             return updatedCart;
+    //         } else {
+    //             return [
+    //                 ...prevCart,
+    //                 { ...Ingredient, IngredientAmount: Action === "Add" ? 1 : 0 }
+    //             ];
+    //         }
+    //     });
+    // }
+
+    const cartChange = (Ingredient: IngredientProps, Action: string) => {
+        setCart((prevCart) => {
+            const existingItemIndex = prevCart.findIndex((item) => item.IngredientID === Ingredient.IngredientID);
+
+            if (existingItemIndex >= 0) {
+                // Update the amount of an existing ingredient
+                const updatedCart = [...prevCart];
+                const existingItem = updatedCart[existingItemIndex];
+                const newAmount = Action === "Add" ? existingItem.IngredientAmount + 1 : Math.max(existingItem.IngredientAmount - 1, 0);
+
+                // Update the ingredient with the new amount
+                if (newAmount > 0) {
+                    updatedCart[existingItemIndex] = {
+                        ...existingItem,
+                        IngredientAmount: newAmount,
+                    };
+                    return updatedCart;
+                } else {
+                    // Remove the ingredient if the amount reaches 0
+                    return updatedCart.filter((item) => item.IngredientID !== Ingredient.IngredientID);
+                }
+            } else if (Action === "Add") {
+                // Add the ingredient to the cart if it doesn't already exist and action is "Add"
+                return [...prevCart, { ...Ingredient, IngredientAmount: 1 }];
+            } else {
+                // If action is "Reduce" and the item doesn't exist, do nothing
+                return prevCart;
+            }
+        });
+    };
+
+
+    console.log(Cart);
 
     return (
         <>
@@ -40,8 +99,8 @@ function IngredientsPage() {
                     <p onClick={() => setSelected(0)} className="fw-semibold fs-3">Pick Your Ingredients</p>
                 </Container>
                 <Container fluid className="d-flex flex-row align-items-center justify-content-between w-100 px-5 pb-2" style={{ borderBottom: "1px solid rgb(230, 230, 230)" }}>
-                    {IngredientItems.map((item) => (
-                        <button onClick={() => setSelected(item.ItemNumber)} className={`${Selected == item.ItemNumber ? "active" : ""} navbar-button ps-0 pe-2 d-flex flex-row align-items-center justify-content-between`} style={{ maxWidth: "100px", backgroundColor: "white", maxHeight: "30px" }}>
+                    {IngredientItems.map((item, key) => (
+                        <button key={key} onClick={() => setSelected(item.ItemNumber)} className={`${Selected == item.ItemNumber ? "active" : ""} navbar-button ps-0 pe-2 d-flex flex-row align-items-center justify-content-between`} style={{ maxWidth: "100px", backgroundColor: "white", maxHeight: "30px" }}>
                             <Col className="d-flex flex-row align-items-center justify-content-between">
                                 <div className="d-flex flex-row align-items-center justify-content-center px-1">
                                     <img src={item.LinkIcon} alt={item.Label} width={20} />
@@ -54,8 +113,17 @@ function IngredientsPage() {
                 <Container fluid>
                     <Row>
                         {TempData.map((data, key) => (
-                            <Col md={3} className="d-flex align-items-center justify-content-center mb-5 mt-5">
-                                <IngredientsCard ImageLink={data.ImageLink} IngredientDescription={data.IngredientDescription} IngredientName={data.IngredientName} IngredientWeightPerPorsion={data.IngredientWeightPerPorsion} NutrientsContained={data.NutrientsContained} TotalCalorie={data.TotalCalorie} key={key} />
+                            <Col key={key} md={3} className="d-flex align-items-center justify-content-center mb-5 mt-5">
+                                <IngredientsCard
+                                    IngredientID={data.IngredientID}
+                                    ImageLink={data.ImageLink}
+                                    IngredientDescription={data.IngredientDescription}
+                                    IngredientName={data.IngredientName}
+                                    IngredientWeightPerPorsion={data.IngredientWeightPerPorsion}
+                                    NutrientsContained={data.NutrientsContained}
+                                    TotalCalorie={data.TotalCalorie}
+                                    onAmountChange={cartChange}
+                                />
                             </Col>
                         ))}
                     </Row>
